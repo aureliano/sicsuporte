aba8b7a5861e7aaaae23fd074c84761e5d9e9ea8
aureliano <aureliano.franca@hotmail.com>
2016-05-29 16:30:24 -0300

    Cria layout principal.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+
+  def build_menu_item(key, path)
+    style = 'list-group-item'
+    style << ' active' if (request.path == path)
+
+    link_to t(key), path, class: style
+  end
+
 end
