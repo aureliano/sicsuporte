fd90e8dd323363d41294e9381988aaedf616804e
aureliano <aureliano.franca@hotmail.com>
2016-05-29 22:04:11 -0300

    Constrói ítem de menu vertical.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,10 +1,26 @@
 module ApplicationHelper
 
-  def build_menu_item(key, path)
-    style = 'list-group-item'
-    style << ' active' if (request.path == path)
+  def build_menu_item(key, entity)
+    model = eval(entity.to_s.capitalize).model_name
+    paths = [
+      eval("#{model.plural}_path"),
+      eval("#{model.singular}_path(:id)"),
+      eval("new_#{model.singular}_path"),
+      eval("edit_#{model.singular}_path(:id)")
+    ]
 
-    link_to t(key), path, class: style
+    style = 'list-group-item'
+    target_path = eval("#{model.plural}_path")
+
+    paths.each do |path|
+      mask_path = request.path.sub(/\/\d+\/?/, '/id/').sub(/\/$/, '')
+      if mask_path == path
+        style << ' active'
+        break
+      end
+    end
+
+    link_to t(key), target_path, class: style
   end
 
 end
