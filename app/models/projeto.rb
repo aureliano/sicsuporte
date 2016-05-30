4ce8522bade72285babd61b8765e9b70b39de27c
aureliano <aureliano.franca@hotmail.com>
2016-05-29 23:14:47 -0300

    Configura validações na entidade projeto.

--- app/models/projeto.rb
+++ app/models/projeto.rb
@@ -1,2 +1,9 @@
 class Projeto < ActiveRecord::Base
+  validates :nome, presence: true
+  validates :nome, length: { minimum: 5, maximum: 50 }
+
+  validates :descricao, presence: true
+  validates :descricao, length: { minimum: 5, maximum: 300 }
+
+  validates :integracao_continua, presence: true
 end
--- test/models/projeto_test.rb
+++ test/models/projeto_test.rb
@@ -24,4 +24,98 @@ class ProjetoTest < ActiveSupport::TestCase
     assert_equal 'precadin/', project.integracao_continua
   end
 
+  test 'validate_name_presence' do
+    project = Projeto.new do |p|
+      p.descricao = 'descrição'
+      p.integracao_continua = 'link'
+    end
+
+    assert project.invalid?
+
+    errors = project.errors.messages
+    assert_equal 2, errors[:nome].size
+    assert_equal "não pode ficar em branco", errors[:nome].first
+  end
+
+  test 'validate_name_minimum' do
+    project = Projeto.new do |p|
+      p.nome = 'name'
+      p.descricao = 'descrição'
+      p.integracao_continua = 'link'
+    end
+
+    assert project.invalid?
+
+    errors = project.errors.messages
+    assert_equal 1, errors[:nome].size
+    assert_equal "é muito curto (mínimo: 5 caracteres)", errors[:nome].first
+  end
+
+  test 'validate_name_maximum' do
+    project = Projeto.new do |p|
+      p.nome = ('*' * 51)
+      p.descricao = 'descrição'
+      p.integracao_continua = 'link'
+    end
+
+    assert project.invalid?
+
+    errors = project.errors.messages
+    assert_equal 1, errors[:nome].size
+    assert_equal "é muito longo (máximo: 50 caracteres)", errors[:nome].first
+  end
+
+  test 'validate_descricao_presence' do
+    project = Projeto.new do |p|
+      p.nome = 'project'
+      p.integracao_continua = 'link'
+    end
+
+    assert project.invalid?
+
+    errors = project.errors.messages
+    assert_equal 2, errors[:descricao].size
+    assert_equal "não pode ficar em branco", errors[:descricao].first
+  end
+
+  test 'validate_descricao_minimum' do
+    project = Projeto.new do |p|
+      p.nome = 'project'
+      p.descricao = '1234'
+      p.integracao_continua = 'link'
+    end
+
+    assert project.invalid?
+
+    errors = project.errors.messages
+    assert_equal 1, errors[:descricao].size
+    assert_equal "é muito curto (mínimo: 5 caracteres)", errors[:descricao].first
+  end
+
+  test 'validate_descricao_maximum' do
+    project = Projeto.new do |p|
+      p.nome = 'project'
+      p.descricao = ('*' * 301)
+      p.integracao_continua = 'link'
+    end
+
+    assert project.invalid?
+
+    errors = project.errors.messages
+    assert_equal 1, errors[:descricao].size
+    assert_equal "é muito longo (máximo: 300 caracteres)", errors[:descricao].first
+  end
+
+  test 'validate_integracao_continua_presence' do
+    project = Projeto.new do |p|
+      p.nome = 'project'
+      p.descricao = 'description'
+    end
+
+    assert project.invalid?
+
+    errors = project.errors.messages
+    assert_equal 1, errors[:integracao_continua].size
+    assert_equal "não pode ficar em branco", errors[:integracao_continua].first
+  end
 end
